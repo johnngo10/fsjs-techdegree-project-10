f40f0b4fc6fe0c9f0f4bd2eafdd1f56070117504
xuanngo10 <xuan.ngo10@gmail.com>
2020-07-03 22:27:45 -0700

    updated all components

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -25,8 +25,29 @@ const CourseDetail = (props) => {
         <div className="grid-66">
           <div className="course--header">
             <h4 className="course--label">Course</h4>
-            <h3 className="course--title">Build a Basic Bookcase</h3>
-            <p>By Joe Smith</p>
+            <h3 className="course--title">{course.title}</h3>
+            <p>
+              By {course.user.firstName} {course.user.lastName}
+            </p>
+          </div>
+        </div>
+        <div className="course--description">
+          <p>{course.description}</p>
+        </div>
+        <div className="grid-25 grid-right">
+          <div className="course--stats">
+            <ul className="course--stats--list">
+              <li className="course--stats--list--item">
+                <h4>Estimated Time</h4>
+                <h3>{course.estimatedTime}</h3>
+              </li>
+              <li className="course--stats--list--item">
+                <h4>Materials Needed</h4>
+                <ul>
+                  <li>{course.materialsNeeded}</li>
+                </ul>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
