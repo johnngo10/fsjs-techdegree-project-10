3a15bfed64504737297703888cbab38a05c56244
xuanngo10 <xuan.ngo10@gmail.com>
2020-07-04 22:04:28 -0700

    progress

---
+++ client/src/components/Form.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+export default (props) => {
+  const { cancel, errors, submit, submitButtonText, elements } = props;
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    submit();
+  }
+
+  function handleCancel(event) {
+    event.preventDefault();
+    cancel();
+  }
+
+  return (
+    <div>
+      <ErrorsDisplay errors={errors} />
+      <form onSubmit={handleSubmit}>
+        {elements()}
+        <div class="grid-100 pad-bottom">
+          <button class="button" type="submit">
+            Sign Up
+          </button>
+          <Link to="/">
+            <button type="button" class="button button-secondary">
+              Cancel
+            </button>
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+function ErrorsDisplay({ errors }) {
+  let errorsDisplay = null;
+
+  if (errors.length) {
+    errorsDisplay = (
+      <div>
+        <h2 className="validation--errors--label">Validation errors</h2>
+        <div className="validation-errors">
+          <ul>
+            {errors.map((error, i) => (
+              <li key={i}>{error}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+
+  return errorsDisplay;
+}
